13cfb310e4aa054a16caaa45e2e5b2ed3dffa121
Waqas Khan <wuikhan@gmail.com>
2018-06-04 23:54:10 -0400

    initial commit

---
+++ 01_Environment_Setup.py
@@ -0,0 +1,4 @@
+# activate the environment
+# 'conda env list' to check the list of all available environments
+# 'source activate /anaconda3/envs/PFDS'
+# 'conda list' to view all the installed packages
