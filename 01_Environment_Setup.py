a23767cc88f926f3786496b321c80ee8e9c914fd
Waqas Khan <wuikhan@gmail.com>
2018-06-20 11:37:41 -0400

    python program will open a browser after every 10 esconds

--- 01_Environment_Setup.py
+++ 01_Environment_Setup.py
@@ -2,3 +2,18 @@
 # 'conda env list' to check the list of all available environments
 # 'source activate /anaconda3/envs/PFDS'
 # 'conda list' to view all the installed packages
+
+
+import webbrowser
+import  time
+
+total_break = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+
+while(break_count < total_break):
+    time.sleep(10)
+    webbrowser.open("http://www.google.com")
+    break_count = break_count + 1
+
