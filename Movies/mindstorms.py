4c5733892e5d9b3dc4c9f4c435e6d8d3b0ab9923
Waqas Khan <wuikhan@gmail.com>
2018-06-25 13:04:25 -0400

    square box

--- Movies/mindstorms.py
+++ Movies/mindstorms.py
@@ -6,6 +6,16 @@ def draw_square():
 
     brad = turtle.Turtle()
     brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
+
+    brad.forward(100)
+    brad.right(90)
 
     window.exitonclick()
 
