b4f2c84ddd8a0aaa706d79a5349af39e240a42cc
Waqas Khan <wuikhan@gmail.com>
2018-06-25 13:32:54 -0400

    separated in diff methods

--- Movies/mindstorms.py
+++ Movies/mindstorms.py
@@ -1,23 +1,27 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
+
     window = turtle.Screen()
-    window.bgcolor("red")
-
+    window.bgcolor("black")
+#create the turtle brad - Draws a square
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    draw_square(brad)
+# create the turtle Angie - Draws a circle
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
 
     window.exitonclick()
 
 
-draw_square()
+draw_art()
